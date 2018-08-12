d16e6bd262d99f04fd9263c6449a638552f77d7e
Caroline Berg <carolineeberg@gmail.com>
2018-08-12 20:40:42 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -38,9 +38,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-		 puts alan_kay_is_known_for[:alan_kay][:known_for]
+		
 end
-
+ puts alan_kay_is_known_for[:alan_kay][:known_for]
 
 def dennis_ritchies_language
 	programmer_hash =
