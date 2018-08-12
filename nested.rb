4fec11ae9c2a512886e95c1ce9f412efa8c47a10
Caroline Berg <carolineeberg@gmail.com>
2018-08-12 20:55:02 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -130,7 +130,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
 
-adding_to_dennis[:dennis_ritchie][:languages] << "Assembly"
+
